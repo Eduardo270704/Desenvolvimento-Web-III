0045618979451c56292d779cca6f054878dbb318
Eduardo Moraes <luis270704@gmail.com>
2024-03-14 21:03:35 -0300

    atividade 2 feita

--- aula4/previsao/test/previsao.test.ts
+++ aula4/previsao/test/previsao.test.ts
@@ -7,7 +7,7 @@ jest.mock("../src/services/Cptec", () => {
     .mockImplementationOnce(() => {
       return {
         listaCidades: jest.fn().mockImplementation(() => {
-          return "<note><cidade>teste</cidade><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>";
+          return "<note><cidade>teste</cidade><from>Jani</from><heading>Reminder</heading><body></body></note>";
         }),
       };
     })
